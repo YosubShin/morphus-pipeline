e01c0188af37d84949655c18f7d1f9077630b0ac
Yosub Shin <yosub@yosubshin.com>
2015-04-20 19:28:26 -0500

    update

--- coordinator.py
+++ coordinator.py
@@ -135,7 +135,7 @@ def run_experiment(pf, hosts, overall_target_throughput, workload_type, total_nu
 
     logger.debug('Cassandra deploy threads finished with outputs: %s...' % output)
     proc = subprocess.Popen(
-        'ssh %s %s/bin/nodetool status -h %s | grep 10.1.1' % (seed_host, cassandra_path, seed_host),
+        ['ssh', seed_host, '%s/bin/nodetool status -h %s | grep 10.1.1' % (cassandra_path, seed_host)],
         stdout=subprocess.PIPE)
     host_statuses = {}
     hosts_down = False
